4da0710c88c2e50df5c4cc593ebcace918417699
joaominosso <kallarari@gmail.com>
2023-08-25 09:48:07 -0300

    bugfix(metadata)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,10 @@ import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
-
+export const metadata: Metadata = {
+  title: 'My website',
+  description: 'this is my website',
+}
 
 export default function RootLayout({
   children,
