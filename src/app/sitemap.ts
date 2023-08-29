425b4d76a18d531ca3ed88ace08718750b3e2807
joaominosso <kallarari@gmail.com>
2023-08-29 09:29:29 -0300

    bugfix(test):robots test

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,38 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [/* 
+  return [
     {
-      url: process.env.PORTFOLIO_URL!,
+      url: "https://portfolio-kallarari.vercel.app/"!,
       lastModified: new Date(),
     },
     {
-      url: process.env.PORTFOLIO_URL + 'aboutMe',
+      url: "https://portfolio-kallarari.vercel.app/" + 'aboutMe',
       lastModified: new Date(),
     },
     {
-      url: process.env.PORTFOLIO_URL + 'bestBugs',
+      url: "https://portfolio-kallarari.vercel.app/" + 'bestBugs',
       lastModified: new Date(),
     },
     {
-      url: process.env.PORTFOLIO_URL + 'profile',
+      url: "https://portfolio-kallarari.vercel.app/" + 'robots.txt',
       lastModified: new Date(),
     },
     {
-      url: process.env.PORTFOLIO_URL + 'robots.txt',
+      url: "https://portfolio-kallarari.vercel.app/" + 'projects',
       lastModified: new Date(),
     },
     {
-      url: process.env.PORTFOLIO_URL + 'projects',
+      url: "https://portfolio-kallarari.vercel.app/" + 'contact',
       lastModified: new Date(),
-    },
+    }, 
     {
-      url: process.env.PORTFOLIO_URL + 'contact',
+      url: "https://portfolio-kallarari.vercel.app/" + 'copyright/privacy',
       lastModified: new Date(),
-    }, */
+    }, 
+    {
+      url: "https://portfolio-kallarari.vercel.app/" + 'copyright/terms',
+      lastModified: new Date(),
+    }
   ];
 }
